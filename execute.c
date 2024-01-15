1fb6dcfd22554b3505c98656229726eab5c410d5
vnporras <vnporras1028@gmail.com>
2024-01-14 16:31:44 -0800

    modified the code

--- execute.c
+++ execute.c
@@ -7,29 +7,14 @@
 
 void execute(char *userCommand)
 {
-	int initialSize = 50;
 	int argCount = 0;
 	char *token;
 	pid_t pid;
-	char **args;
+	char *args[1024];
 
-	args = malloc(initialSize *sizeof(char *));
-	if (args == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
 	token = strtok(userCommand, " ");
-	while (token != NULL)
+	while (token != NULL && argCount < 1023)
 	{
-		if (argCount >= initialSize - 1)
-		{
-			initialSize *= 2;
-			args = realloc(args, initialSize * sizeof(char *));
-			if (args == NULL)
-			{
-				exit(EXIT_FAILURE);
-			}
-		}
 		args[argCount++] = token;
 		token = strtok(NULL, " ");
 	}
